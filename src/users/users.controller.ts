492635db2bcbfe2d32aa7e9526a75ff5c641ad3c
Ferenc Koler <ferenckoler@outlook.com>
2023-11-20 20:55:27 +0100

    Service and Controller basic structure

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,41 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService) { }
+
     @Get()
-    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-        return [];
+    findAll(@Query('instrument') instrument?: 'Vocal' | 'Guitar' | 'Drums' | 'Keyboards' | 'Bass') {
+        return this.usersService.findAll(instrument);
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return { id };
+        return this.usersService.findOne(+id);
     }
 
     @Post()
-    create(@Body() user: {}) {
-        return user;
+    create(@Body() user: {
+        first_name: string,
+        last_name: string,
+        instrument: 'Vocal' | 'Guitar' | 'Drums' | 'Keyboards' | 'Bass',
+    }) {
+        return this.usersService.create(user);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate };
+    update(@Param('id') id: string, @Body() userUpdate: {
+        first_name?: string,
+        last_name?: string,
+        instrument?: 'Vocal' | 'Guitar' | 'Drums' | 'Keyboards' | 'Bass',
+    }) {
+        return this.usersService.update(+id, userUpdate);
     }
 
     @Delete(':id')
     delete(@Param('id') id: string) {
-        return { id };
+        return this.usersService.delete(+id);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,97 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "first_name": "Till",
+            "last_name": "Lindemann",
+            "instrument": "Vocal"
+        },
+        {
+            "id": 2,
+            "first_name": "Paul",
+            "last_name": "Landers",
+            "instrument": "Guitar"
+        },
+        {
+            "id": 3,
+            "first_name": "Christoph",
+            "last_name": "Schneider",
+            "instrument": "Drums"
+        },
+        {
+            "id": 4,
+            "first_name": "Christian",
+            "last_name": "Lorenz",
+            "instrument": "Keyboards"
+        },
+        {
+            "id": 5,
+            "first_name": "Richard",
+            "last_name": "Kruspe",
+            "instrument": "Guitar"
+        },
+        {
+            "id": 6,
+            "first_name": "Oliver",
+            "last_name": "Riedel",
+            "instrument": "Bass"
+        }
+    ];
+
+    findAll(instrument?: 'Vocal' | 'Guitar' | 'Drums' | 'Keyboards' | 'Bass') {
+
+        if (instrument) {
+            return this.users.filter(user => user.instrument === instrument);
+        }
+        return this.users
+    }
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id);
+
+        return user;
+    }
+
+    create(user: {
+        first_name: string,
+        last_name: string,
+        instrument: 'Vocal' | 'Guitar' | 'Drums' | 'Keyboards' | 'Bass',
+    }) {
+        const usersByHighestId = [...this.users]
+            .sort((a, b) => b.id - a.id);
+
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...user,
+        };
+
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    update(id: number, updatedUser: {
+        first_name?: string,
+        last_name?: string,
+        instrument?: 'Vocal' | 'Guitar' | 'Drums' | 'Keyboards' | 'Bass',
+    }) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updatedUser }
+            }
+            return user;
+        })
+
+        return this.findOne(id);
+    };
+
+    delete(id: number) {
+        const deletedUser = this.findOne(id);
+
+        this.users = this.users.filter(user => user.id !== id);
+
+        return deletedUser;
+    }
+}
